a893da1d4d6f978cb29c66f2680787a81a2e514b
Idris Junaid <Junaid.idrisabiodun@gmail.com>
2022-06-20 10:31:00 +0100

    A new django project and blog app created to replace the earlier one created

---
+++ I4G000904SXE/blog/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from datetime import datetime
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Post(models.Model):
+    Title = models.CharField(max_length=200)
+    Text = models.TextField()
+    Author = models.ForeignKey(User, on_delete=models.CASCADE)
+    Created_date = models.DateTimeField(default=datetime.now)
+    Published_date = models.DateTimeField(default=datetime.now)
+ 
+    def __str__(self) -> str:
+        return self.Title
+ 
+    def __str__(self) -> str:
+        return self.Text
